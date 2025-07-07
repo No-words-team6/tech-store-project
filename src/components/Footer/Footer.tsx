d73e5cee1385e2262a7b7499cf26fbc28c801d70
Gregory Korsun <lvlup9119@gmail.com>
2025-07-07 20:36:14 +0300

    NPC-10: fix styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -12,39 +12,37 @@ const isLinkActive = ({ isActive }: { isActive: boolean }) =>
 
 export const Footer = () => {
   return (
-    <div className="container back-color">
-      <footer className="flex justify-between items-center w-[94.66%] h-24">
-        <a href="#" className="px-4 py-2">
-          <img src={logo} alt="Nice Gadgets" className="h-6 w-auto" />
-        </a>
+    <footer className="flex justify-between items-center h-24 back-color">
+      <a href="#" className="px-4 py-2">
+        <img src={logo} alt="Nice Gadgets" className="h-6 w-auto" />
+      </a>
 
-        <div className="flex gap-x-20 uppercase">
-          <a
-            href="https://github.com/No-words-team6/tech-store-project"
-            target="_blank"
-            rel="noreferrer"
-            className="font-mont font-bold text-white"
-          >
-            Github
-          </a>
-
-          <NavLink to="contacts" className={isLinkActive}>
-            Contacts
-          </NavLink>
-
-          <NavLink to="rights" className={isLinkActive}>
-            Rights
-          </NavLink>
-        </div>
-
+      <div className="flex gap-x-20 uppercase">
         <a
-          href="#"
-          className="flex gap-x-4 items-center hover:underline-offset-1"
+          href="https://github.com/No-words-team6/tech-store-project"
+          target="_blank"
+          rel="noreferrer"
+          className="font-mont font-bold text-white hover:text-gray-400"
         >
-          <p className="text-gray-600">Back to top</p>
-          <ChevronUp className="w-8 h-8 bg-gray-600 white" />
+          Github
         </a>
-      </footer>
-    </div>
+
+        <NavLink to="contacts" className={isLinkActive}>
+          Contacts
+        </NavLink>
+
+        <NavLink to="rights" className={isLinkActive}>
+          Rights
+        </NavLink>
+      </div>
+
+      <a
+        href="#"
+        className="flex gap-x-4 items-center hover:underline-offset-1"
+      >
+        <p className="text-gray-600">Back to top</p>
+        <ChevronUp className="w-8 h-8 bg-gray-600 white" />
+      </a>
+    </footer>
   );
 };
