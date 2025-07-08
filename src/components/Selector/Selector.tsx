4bc691f16018ce7d35a3b282b99b7fec8d0a130e
Gregory Korsun <lvlup9119@gmail.com>
2025-07-08 21:28:59 +0300

    NPC-18: develop sort selectors

--- src/components/Selector/Selector.tsx
+++ src/components/Selector/Selector.tsx
@@ -1,4 +1,3 @@
-import type { SortBy } from '@/types';
 import {
   Select,
   SelectContent,
@@ -7,39 +6,67 @@ import {
   SelectTrigger,
   SelectValue,
 } from '../ui/select';
+import { useSearchParams } from 'react-router-dom';
+import type { SortBy } from '@/types';
 import type { TimesItems } from '@/types/TimesItems';
 
 interface SelectorProps {
   title: string;
-  items: SortBy[] | TimesItems[];
-  selectorWidth: string;
+  options: SortBy[] | TimesItems[];
+  keySelectedSort: string;
   selectedSort: string;
-  handleSelectChange: (value: string) => void;
+  selectorWidth: string;
 }
 
 export const Selector: React.FC<SelectorProps> = ({
   title,
-  items,
-  selectorWidth = 'w-auto',
+  options,
+  keySelectedSort,
   selectedSort,
-  handleSelectChange,
+  selectorWidth,
 }) => {
-  console.log(selectedSort);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const handleSelectChange = (value: string) => {
+    const newParams = new URLSearchParams(searchParams);
+    newParams.set(keySelectedSort, value);
+    setSearchParams(newParams);
+  };
 
   return (
     <div className="flex flex-col gap-1">
       <p className="font-mont text-xs text-gray-500">{title}</p>
 
       <Select value={selectedSort} onValueChange={handleSelectChange}>
-        <SelectTrigger className={`text-white ${selectorWidth}`}>
-          <SelectValue placeholder={!selectedSort && 'Choose filter'} />
+        <SelectTrigger
+          className={`
+            font-mont
+            text-[14px]
+            ${selectorWidth}
+            text-white
+            bg-gray-700
+            [&>span]:text-white
+            border-0
+            rounded-none
+            hover:border-b-[0.5px]
+            active:border-0
+            focus-visible:ring-0`}
+        >
+          <SelectValue placeholder={!selectedSort && 'Choose'} />
         </SelectTrigger>
 
-        <SelectContent>
+        <SelectContent
+          className="font-mont text-[14px] rounded-none p-0 bg-gray-700 border-0 text-white"
+          style={{ maxHeight: '200px', overflowY: 'auto' }}
+        >
           <SelectGroup>
-            {items.map((item) => (
-              <SelectItem key={item} value={`${item}`}>
-                {item}
+            {options.map((option) => (
+              <SelectItem
+                key={option}
+                value={option}
+                className="rounded-none data-[highlighted]:bg-gray-600 data-[highlighted]:text-white"
+              >
+                {option}
               </SelectItem>
             ))}
           </SelectGroup>
