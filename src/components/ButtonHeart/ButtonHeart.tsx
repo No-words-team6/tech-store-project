2834e798a3252dd5363e499b1a2a505b4d3dabc4
Gregory Korsun <lvlup9119@gmail.com>
2025-07-09 16:05:54 +0300

    NPC-26: add favorite (button like) functionality

--- src/components/ButtonHeart/ButtonHeart.tsx
+++ src/components/ButtonHeart/ButtonHeart.tsx
@@ -1,9 +1,50 @@
+import { Button } from '@/components/ui/button';
+import type { Product } from '@/types';
+import cn from 'classnames';
 import { Heart } from 'lucide-react';
+import { useEffect, useState } from 'react';
 
-export const ButtonHeart = () => {
+interface ButtonHeartProps {
+  product: Product;
+}
+
+export const ButtonHeart: React.FC<ButtonHeartProps> = ({ product }) => {
+  const [isInFavorites, setIsInFavorites] = useState(false);
+
+  useEffect(() => {
+    const storedItem = localStorage.getItem(product.itemId);
+
+    if (storedItem) {
+      const parsedItem = JSON.parse(storedItem);
+
+      if (parsedItem?.itemId === product.itemId) {
+        setIsInFavorites(true);
+      }
+    }
+  }, [product.itemId]);
+
+  const handleToggleHeart = () => {
+    const storedItem = localStorage.getItem(product.itemId);
+
+    if (storedItem) {
+      localStorage.removeItem(product.itemId);
+      setIsInFavorites(false);
+    } else {
+      localStorage.setItem(product.itemId, JSON.stringify(product));
+      setIsInFavorites(true);
+    }
+  };
+
   return (
-    <button className="w-10 h-10 bg-[#323542] hover:bg-[#4A4D58] flex items-center justify-center hover:cursor-pointer">
-      <Heart className="w-4 h-4" />
-    </button>
+    <Button
+      className="w-10 h-10 flex items-center justify-center hover:cursor-pointer rounded-none bg-[#323542] hover:bg-[#4A4D58]"
+      onClick={handleToggleHeart}
+    >
+      <Heart
+        className={cn('w-4 h-4', {
+          'text-red-700 fill-red-700': isInFavorites,
+        })}
+      />
+    </Button>
   );
 };
--- src/pages/FavouritesPage.tsx
+++ src/pages/FavouritesPage.tsx
@@ -1,13 +1,48 @@
 import { BreadcrumbNav } from '@/components/BreadcrumbNav';
+import { ProductCard } from '@/components/ProductCard';
+import { Heart } from 'lucide-react';
 
 export const FavouritesPage = () => {
+  const products = [];
+
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+
+    if (key !== null) {
+      const product = localStorage.getItem(key);
+
+      if (product !== null) {
+        products.push(JSON.parse(product));
+      }
+    }
+  }
+
   return (
     <div className="col-span-24 grid grid-cols-24 gap-x-[16px] pt-[24px] pb-[80px]">
       <BreadcrumbNav />
 
       <div className="col-span-24 grid grid-cols-24 gap-x-[16px] gap-y-[24px]">
         <h1 className="col-span-24 color-white font-mont font-bold text-5xl">
-          Favourites Page Body
+          Favourites
+          <div className="col-span-24 grid grid-cols-24 gap-x-[16px] gap-y-[40px]">
+            {products.length === 0 && (
+              <div className="col-span-24">
+                <p className="font-light text-2xl">
+                  You don`t have any favorites things.
+                </p>
+                <p className="flex font-extralight text-xs">
+                  If you wont to choose someon, you can just click to{' '}
+                  <Heart className="w-3 h-3" />
+                </p>
+              </div>
+            )}
+
+            {products.map((product) => (
+              <div className="col-span-6" key={product.id}>
+                <ProductCard product={product} />
+              </div>
+            ))}
+          </div>
         </h1>
       </div>
     </div>
