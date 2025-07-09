74a8bd6d2973cb6def24e5e6fbfbf7e091d6eb88
AzartiksMan <azartofgod@gmail.com>
2025-07-09 18:25:37 +0300

    add tailwind config and tailwind-animations-lib

---
+++ src/components/ButtonAddToCart/ButtonAddToCart.tsx
@@ -0,0 +1,9 @@
+import { Button } from '../ui/button';
+
+export const ButtonAddToCart = () => {
+  return (
+    <Button className="denis text-[#F1F2F9] w-[160px] h-[40px] text-sm font-bold hover:bg-[#A378FF] hover:cursor-pointer rounded-none">
+      Add to cart
+    </Button>
+  );
+};
