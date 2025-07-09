9f2fadf366a56cffdb83973f48fbc89365b1f9f6
koliiis <ann27052005@gmail.com>
2025-07-09 18:49:27 +0300

    NPC-30: add ProductSlider + fixed ImageSlider

--- src/components/ProductSlider/ProductSlider.tsx
+++ src/components/ProductSlider/ProductSlider.tsx
@@ -3,6 +3,13 @@ import type React from 'react';
 import { ProductCard } from '../ProductCard';
 import { Loader } from '../Loader';
 import { ButtonSwipe } from '../ButtonSwipe';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Autoplay } from 'swiper/modules';
+import type { SwiperClass } from 'swiper/react';
+
+import './ProductSlider.css';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 interface Props {
   productList: Product[];
@@ -15,6 +22,10 @@ export const ProductSlider: React.FC<Props> = ({
   isLoading,
   title,
 }) => {
+  const [swiperInstance, setSwiperInstance] = useState<SwiperClass | null>(
+    null,
+  );
+
   return (
     <section className="col-span-24 grid grid-cols-24 gap-x-[16px] gap-y-[24px]">
       <div className="col-span-24 grid grid-cols-24 gap-x-[16px]">
@@ -22,16 +33,36 @@ export const ProductSlider: React.FC<Props> = ({
           {title}
         </h2>
 
-        <ButtonSwipe />
+        <ButtonSwipe
+          onPrevClick={() => swiperInstance?.slidePrev()}
+          onNextClick={() => swiperInstance?.slideNext()}
+        />
       </div>
 
       {isLoading && <Loader />}
-      <div className="col-span-24 grid grid-cols-24 gap-[16px]">
-        {productList.map((product) => (
-          <div key={product.id} className="col-span-6">
-            <ProductCard product={product} />
-          </div>
-        ))}
+      <div className="col-span-24 grid relative w-full max-w-full h-[432px] mb-[48px]">
+        <Swiper
+          modules={[Pagination, Autoplay]}
+          onSwiper={setSwiperInstance}
+          loop
+          slidesPerView={4}
+          slidesPerGroup={4}
+          spaceBetween={16}
+          className="w-full h-[586px]"
+        >
+          {productList.map((product) => (
+            <SwiperSlide key={product.id}>
+              <div className="relative w-full">
+                <Link
+                  to={`/${product.category}/${product.itemId}`}
+                  className="block w-full"
+                >
+                  <ProductCard product={product} />
+                </Link>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </section>
   );
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -14,18 +14,14 @@ import { ImageSlider } from '@/components/BannerSlider';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
 const latestProductYear = 2022;
 
-const getNewestList = (data: Product[], year: number, limit: number) => {
-  return data
-    .filter((product: Product) => product.year === year)
-    .slice(0, limit);
+const getNewestList = (data: Product[], year: number) => {
+  return data.filter((product: Product) => product.year === year);
 };
 
-const getCheapest = (data: Product[], limit: number) => {
-  return [...data]
-    .sort(
-      (product1: Product, product2: Product) => product1.price - product2.price,
-    )
-    .slice(0, limit);
+const getCheapest = (data: Product[]) => {
+  return [...data].sort(
+    (product1: Product, product2: Product) => product1.price - product2.price,
+  );
 };
 
 export const HomePage = () => {
@@ -41,8 +37,8 @@ export const HomePage = () => {
       .finally(() => setIsLoading(false));
   }, []);
 
-  const newModelsList = getNewestList(data, latestProductYear, 4);
-  const hotPricesList = getCheapest(data, 4);
+  const newModelsList = getNewestList(data, latestProductYear);
+  const hotPricesList = getCheapest(data);
 
   const phonesCount = 95;
   const tabletsCount = 24;
