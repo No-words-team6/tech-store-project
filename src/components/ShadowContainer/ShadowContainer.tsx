648054912dc0a4bbdf97647466e3e2ae2a02cabc
Gregory Korsun <lvlup9119@gmail.com>
2025-07-10 08:23:30 +0300

    NPC-33: structure Footer in enother way

--- src/components/ShadowContainer/ShadowContainer.tsx
+++
@@ -1,9 +0,0 @@
-import type React from 'react';
-
-interface Props {
-  children: React.ReactNode;
-}
-
-export const ShadowContainer: React.FC<Props> = ({ children }) => {
-  return <div className="shadow-[0_-1px_0_0_#323542]">{children}</div>;
-};
