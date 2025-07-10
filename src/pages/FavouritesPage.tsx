4c078b5e12e21cd8bca331c202bd650cb2ecfa25
koliiis <ann27052005@gmail.com>
2025-07-10 16:22:55 +0300

    NPC-38: add angry heart icon + fixed styles

--- src/pages/FavouritesPage.tsx
+++ src/pages/FavouritesPage.tsx
@@ -4,6 +4,8 @@ import type { Product } from '@/types';
 import { getFromLocale } from '@/utils/storageService';
 import { useEffect, useState } from 'react';
 
+import placeholder from '../../public/img/placeholder-angry-heart.png';
+
 export const FavouritesPage = () => {
   const [products, setProducts] = useState<Product[]>([]);
 
@@ -18,16 +20,30 @@ export const FavouritesPage = () => {
     <>
       <BreadcrumbNav />
 
-      <div className="col-span-24 grid gap-y-[24px]">
-        <h1 className="col-span-24 color-white font-mont font-bold text-5xl">
+      <div className="col-span-24 grid">
+        <h1 className="col-span-24 color-white font-mont font-bold text-5xl mb-6">
           Favourites
         </h1>
 
-        <p className="col-span-24 font-extralight text-[14px] text-gray-600">
+        <p className="col-span-24 font-extralight text-[14px] text-[#75767F]">
           {productsLength} items
         </p>
 
-        <div className="col-span-24 grid grid-cols-24 gap-x-[16px] gap-y-[40px]">
+        {!productsLength && (
+          <div className="col-span-24 grid grid-cols-24 place-items-center">
+            <img
+              src={placeholder}
+              alt="empty bag"
+              className="col-span-4 col-start-11"
+            />
+
+            <h2 className="col-span-6 col-start-10 font-mont text-[#515151] mt-[-20px]">
+              You don&apos;t have any favourite items :(
+            </h2>
+          </div>
+        )}
+
+        <div className="col-span-24 grid grid-cols-24 gap-x-[16px] gap-y-[40px] mt-6">
           {products.map((product) => (
             <div className="col-span-6" key={product.id}>
               <ProductCard product={product} />
