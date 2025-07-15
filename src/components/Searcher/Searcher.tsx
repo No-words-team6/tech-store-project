a1174bba13af878d5f6be660666099a94534a3db
Gregory Korsun <lvlup9119@gmail.com>
2025-07-15 15:42:43 +0300

    NPC-64: improve debounce on search

--- src/components/Searcher/Searcher.tsx
+++ src/components/Searcher/Searcher.tsx
@@ -1,4 +1,10 @@
-import { useEffect, useRef, useState } from 'react';
+import {
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+  type ChangeEvent,
+} from 'react';
 import { Input } from '../ui/input';
 import { useProductStore } from '@/stores/productStore';
 import { Search } from 'lucide-react';
@@ -6,6 +12,7 @@ import type { Product } from '@/types';
 import { useSearchParams } from 'react-router-dom';
 import { useCastomNavigator } from '@/hooks/useCastomNavigator';
 import { filterProductSearchs } from '@/utils/filterProductSearchs';
+import debounce from 'lodash.debounce';
 
 export const Searcher = () => {
   const products = useProductStore((state) => state.products);
@@ -14,13 +21,18 @@ export const Searcher = () => {
   const currentBrand = searchParams.get('brand') ?? '';
 
   const [query, setQuery] = useState('');
+  const [appliedQuery, setAppliedQuery] = useState('');
   const ref = useRef<HTMLDivElement>(null);
   const navigate = useCastomNavigator();
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const applyQuery = useCallback(debounce(setAppliedQuery, 400), []);
 
   useEffect(() => {
     function handleClickOutside(event: MouseEvent) {
       if (ref.current && !ref.current.contains(event.target as Node)) {
         setQuery('');
+        setAppliedQuery('');
       }
     }
 
@@ -31,13 +43,19 @@ export const Searcher = () => {
   }, [ref]);
 
   const filteredProducts = filterProductSearchs(products, {
-    query,
+    appliedQuery,
     currentBrand,
   });
 
   const handleOnClick = (product: Product) => {
     navigate(`/${product.category}/${product.itemId}`);
   };
+
+  const handleQueryChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value.trimStart());
+    applyQuery(event.target.value.trimStart());
+  };
+
   return (
     <div ref={ref}>
       <div className="relative flex min-w-45">
@@ -46,11 +64,11 @@ export const Searcher = () => {
         <Input
           className="pl-10 text-white rounded-none"
           value={query}
-          onChange={(event) => setQuery(event.target.value.trimStart())}
+          onChange={handleQueryChange}
         />
       </div>
 
-      {query.length > 0 && filteredProducts.length > 0 && (
+      {appliedQuery.length > 0 && filteredProducts.length > 0 && (
         <div className="absolute bg-gray-800 py-3">
           <div className=" flex flex-col gap-2 max-h-64 px-3 overflow-y-scroll ">
             {filteredProducts.map((product) => {
